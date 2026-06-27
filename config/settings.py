d6f52853d5a7c3c531ef02727611a4c21ca8344d
AleksArtemiy <alekseev3artem@gmail.com>
2026-06-27 14:20:25 +0300

    Change database settings to PostgreSQL
    Updated database configuration to use PostgreSQL.

--- config/settings.py
+++ config/settings.py
@@ -82,24 +82,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'minobr',
-#         'USER': 'postgres',
-#         'PASSWORD': '15112005',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+Database
+https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'minobr',
+        'USER': 'postgres',
+        'PASSWORD': '15112005',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
